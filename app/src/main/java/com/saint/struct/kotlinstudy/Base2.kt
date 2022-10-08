64e5f9af4884ef7f61f5aea5e499d39b15522ee1
wshfzhwj <wshfzhwj@126.com>
2022-10-08 14:10:04 +0800

    test process (#3)

--- app/src/main/java/com/saint/struct/kotlinstudy/Base2.kt
+++ app/src/main/java/com/saint/struct/kotlinstudy/Base2.kt
@@ -2,7 +2,7 @@ package com.saint.struct.kotlinstudy
 
 open class Base2(val name: String) {
 
-    init { println("Initializing Base") }
+    init { println("Initializing Base study") }
 
     open val size: Int =
         name.length.also { println("Initializing size in Base: $it") }
