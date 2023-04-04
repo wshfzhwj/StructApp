c26417badd8db5094c60ddfa8daf24f24923b94c
wshfzhwj <wshfzhwj@126.com>
2023-04-04 15:44:22 +0800

    change build.gradle style

---
+++ app/src/main/java/com/saint/struct/StructApp.kt
@@ -0,0 +1,11 @@
+package com.saint.struct
+
+import android.app.Application
+
+class StructApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //        CrashHandler crashHandler = new CrashHandler();
+//        Thread.setDefaultUncaughtExceptionHandler(crashHandler);
+    }
+}
