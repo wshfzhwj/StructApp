b00f26711846034092408c8e32473ead8cbf12da
wshfzhwj <wshfzhwj@126.com>
2023-04-06 16:23:46 +0800

    test (#18)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.libs.versions.toml
 //方式一
+//方式一2
 //@Suppress("DSL_SCOPE_VIOLATION")  //添加DSL_SCOPE_VIOLATION,处理alias报错
 //plugins {
 //    //apply false 表示在父项目中声明，但是父项目不会立即加载，可以在子项目中通过ID的方式进行使用
