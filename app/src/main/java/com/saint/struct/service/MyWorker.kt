c7c6d375fc3f0c858cf12c961067a6ec00241136
wshfzhwj <wshfzhwj@126.com>
2023-06-27 09:08:37 +0800

    refactor coroutines (#29)

--- app/src/main/java/com/saint/struct/service/MyWorker.kt
+++ app/src/main/java/com/saint/struct/service/MyWorker.kt
@@ -2,17 +2,19 @@ package com.saint.struct.service
 
 import android.content.Context
 import android.util.Log
+import androidx.work.CoroutineWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
-class MyWorker (context: Context, workerParameters: WorkerParameters): Worker(context, workerParameters) {
-    override fun doWork(): Result {
-        Log.e("TEST", "Checking system。。。。。。。。")
-        Thread.sleep(3000)
-        Log.e("TEST", "Checking system done.")
-
-        return Result.success()
+class MyWorker(context: Context, workerParameters: WorkerParameters) : CoroutineWorker(context, workerParameters) {
+    override suspend fun doWork(): Result {
+        try {
+            Log.e("TEST", "Checking system。。。。。。。。")
+            Thread.sleep(3000)
+            Log.e("TEST", "Checking system done.")
+            return Result.success()
+        } catch (e: InterruptedException) {
+            return Result.failure()
+        }
     }
-
-
 }
